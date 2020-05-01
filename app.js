87dd37f99c8c63dac0540ba6cdc039927c1d4cb7
yogesharora3 <yogesharora375@gmail.com>
2020-05-01 16:57:04 +0530

    working

--- app.js
+++ app.js
@@ -11,17 +11,48 @@ loadEventlistner();
 
 //add event listener
 function loadEventlistner(){
+    //adding event on loading
+    document.addEventListener("DOMContentLoaded",handleLoad)
+    //add event listener
     form.addEventListener('submit',addTask);
+    //removing item using event
     collection.addEventListener('click',removeItem);
+    //filtering the list 
     filter.addEventListener('keyup',handleFilter);
+    //clearing all the list
+    btnClear.addEventListener('click',handleClear);
 }
+//load list on opening the web page from local storage
+function handleLoad(){
+    let tasks;
+    if(localStorage.getItem('tasks')==null){
+        tasks=[];
+    }
+    else{
+        tasks=JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function(task){
+        const list = document.createElement('li');
+       list.className='collection-item';
+       list.appendChild(document.createTextNode(task));
+       const link=document.createElement('a');
+       link.id="collection-link";
+       link.className=" right";
+       link.innerHTML="<i class=\"fa fa-times \" aria-hidden=\"true\" style=\"font-size:1.erm\"></i>"
+       list.appendChild(link);
+       collection.append(list);
+    })
+
+}
+//function which add task in the list
 function addTask(e){
     e.preventDefault();
+    //checking  whether the field is empty or not
     if(task.value===''){
         alert("can not be empty");
         task.focus();
     }
-    else{
+    else{  //adding task into the list
        const list = document.createElement('li');
        list.className='collection-item';
        list.appendChild(document.createTextNode(task.value));
@@ -31,15 +62,47 @@ function addTask(e){
        link.innerHTML="<i class=\"fa fa-times \" aria-hidden=\"true\" style=\"font-size:1.erm\"></i>"
        list.appendChild(link);
        collection.append(list);
+       //adding task into the local storage
+       addToLocalStorage(task.value);
        task.value="";
     }
+}
+function addToLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks')==null){
+        tasks=[];
+    }
+    else{
+        tasks=JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+    localStorage.setItem('tasks',JSON.stringify(tasks));
 }
 function removeItem(e){
     if(e.target.parentElement.classList.contains("right")){
         if(confirm("you want to delete ?")){
             e.target.parentElement.parentElement.remove();
+            deleteFromLocalStorage(e.target.parentElement.parentElement);
         }
     }
+}
+function deleteFromLocalStorage(e){
+    let tasks;
+    if(localStorage.getItem('tasks')==null){
+        tasks=[];
+    }
+    else{
+        tasks=JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function(val,index){
+        if(val===e.textContent){
+            console.log(index);
+            tasks.splice(index,1);
+        }
+    })
+    // localStorage.setItem('tasks',tasks);
+    console.log(tasks);
+    localStorage.setItem('tasks',JSON.stringify(tasks));
 }
 function handleFilter(e){
     text=e.target.value.toLowerCase();
@@ -55,4 +118,12 @@ function handleFilter(e){
         }
     })
 
+}
+function handleClear(e){
+    // list=querySelectorAll('.collection-list')
+   var menu= document.querySelector('.collection');
+   while(menu.firstElementChild){
+       menu.removeChild(menu.firstElementChild);
+   }
+   localStorage.clear();
 }
